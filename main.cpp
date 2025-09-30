13296038094921a7e42569b7a18583be815721d5
josem-2415 <jose.cardona.gil@correounivalle.edu.co>
2025-09-30 16:31:36 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,13 @@ int main() {
 
     while (true) {
         BloodDatabase::clearConsole();
+        std::cout <<
+             " ░█████╗░██████╗░██╗░░░██╗███████╗  ██████╗░░█████╗░░░░░░██╗░█████╗░\n"
+             " ██╔══██╗██╔══██╗██║░░░██║╚════██║  ██╔══██╗██╔══██╗░░░░░██║██╔══██╗\n"
+             " ██║░░╚═╝██████╔╝██║░░░██║░░███╔═╝  ██████╔╝██║░░██║░░░░░██║███████║\n"
+             " ██║░░██╗██╔══██╗██║░░░██║██╔══╝░░  ██╔══██╗██║░░██║██╗░░██║██╔══██║\n"
+             " ╚█████╔╝██║░░██║╚██████╔╝███████╗  ██║░░██║╚█████╔╝╚█████╔╝██║░░██║\n"
+             " ░╚════╝░╚═╝░░╚═╝░╚═════╝░╚══════╝  ╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚═╝\n";
         std::cout << "1. Registrar donante\n";
         std::cout << "2. Buscar donante\n";
         std::cout << "3. Eliminar donante\n";
@@ -44,6 +51,21 @@ int main() {
         case 4:
             reporte.loadDataBase(database.getDonors());
             std::cout << "Total registros: " << reporte.donorTotal() << endl;
+            
+            std::cout << "El total de registros del Valle del Cauca, 43 años y B+ es: "<<reporte.donorTotal(d,a,b)<<endl<<endl;
+            
+            d=8;b=1;std::cout << "El total de registros de Chocoanos A+ es: "<<reporte.donorTotal(d,b)<<endl<<endl;
+            d=9;b=8;std::cout << "El total de registros de Aracaunos O- es: "<<reporte.donorTotal(d,b)<<endl<<endl;
+            d=3;b=3;std::cout << "El total de registros de Valle Caucanos B+ es: "<<reporte.donorTotal(d,b)<<endl<<endl;
+            
+            double auxedad;d=8;auxedad=reporte.donorMeanAge(d);
+            d=9;auxedad=(auxedad+reporte.donorMeanAge(d))/2;
+            d=3;auxedad=(auxedad+reporte.donorMeanAge(d))/2;
+            std::cout << "El promedio de edad los individuos de Choco, Arauca y Valle de Cauca: "<<auxedad<<endl<<endl;
+            
+            d=3;b=3;
+            std::cout << "El total de registros mayores de edad vallecaucanos B+: "<<reporte.donorAdult(d,b)<<endl<<endl;
+            
             std::cout << "Ingrese nombre para ver unidades donadas: ";
             std::getline(std::cin, donorName);
             reporte.bloodDonorUnits(donorName);
