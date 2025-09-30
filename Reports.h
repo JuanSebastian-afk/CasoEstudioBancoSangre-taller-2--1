b412b6d108d3c46b338576d45f45c8c1e3db8069
JuanSebastian-afk <juan.quinchua@correounivalle.edu.co>
2025-09-30 18:23:01 -0500

    Update Reports.h

--- Reports.h
+++ Reports.h
@@ -1,3 +1,32 @@
+/*
+Clase: Report
+
+Curso: Fundamentos de Programación Orientada a Objetos (FPOO)
+Universidad del Valle
+
+Información CRC:
+
+Clase: Donante
+
+Responsabilidades:
+    - Ofrecer La cantidad de personas a procesar total, y separada de acuerdo 
+      con su tipo de sangre, procedencia y edad.
+    - Ofrece datos estadísticos sobre el número de chocoanos con tipo de sangre A+,
+      el número de araucanos con tipo de sangre O- y el número de caucanos con 
+      tipo de sangre B+
+    - ofrece datos estadísticos sobre la edad promedio de los donantes del Choco, 
+      Arauca y Valle del Cauca.
+    - ofrece datos estadísticos sobre la cantidad de vallecaucanos con tipo de 
+      sangre B+ y mayores de edad.
+    - Hallar y muestra el número de unidades de sangre que dona una persona y
+      la fecha de donación. 
+    - Entrega un reporte de cierre de año, en el cual se presente el porcentaje 
+      de donantes por tipos de sangre y para cada una de las zonas del país.
+Colaboradores:
+    - Usa la clase BloodDatabase importando su vector de donantes para calcular
+      los datos estadísticos de sus responsabilidades.
+*/
+
 #ifndef REPORTS_H
 #define REPORTS_H
 
