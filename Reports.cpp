ed7abd7cbb0a176a6033a0c89d18d48e2e31bfb5
josem-2415 <jose.cardona.gil@correounivalle.edu.co>
2025-09-30 17:14:55 -0500

    Update Reports.cpp

--- Reports.cpp
+++ Reports.cpp
@@ -50,7 +50,7 @@ int Reports::donorAdult(const int& district, const int& bloodType) const {
 
 int Reports::bloodDonorUnits(const std::string& donorName) const {
     for (const auto& d : auxDonor) {
-        if (d.getId() == donorId) {
+        if (d.getName() == donorName) {
             std::cout << "Donante: " << d.getName() << "\n";
             std::cout << "Unidades donadas: " << d.getUnits() << "\n";
             std::cout << "Fecha última donación: " << d.getDate() << "\n";
